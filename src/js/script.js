d8cbe734a6802bab0efb7b54ad9f4ab3e2af7ea0
Dom <dominic.knabe@gmail.com>
2025-05-11 19:52:17 +1000

    local storage loaded when startup

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@ const addButton = document.querySelector(".addTask__button");
 const taskInput = document.querySelector(".addTask__input");
 const taskList = document.querySelector(".taskList");
 
+document.addEventListener("DOMContentLoaded", loadTasks);
+
 function addTask() {
   const taskText = taskInput.value.trim();
   if (taskText === "") {
@@ -10,14 +12,18 @@ function addTask() {
   }
   const newTask = document.createElement("div");
   newTask.classList.add("tasklist__task");
-  newTask.innerHTML = `<input type="checkbox" name="task" class="tasklist__checkbox" />
+  newTask.innerHTML = `
+  <div class="tasklist__all">
+  <input type="checkbox" name="task" class="tasklist__checkbox" />
     <p>${taskText}</p>
-    <span class="tasklist__edit"><i class="ri-edit-2-line"></i></span>
-              <span class="tasklist__delete"> <i class="ri-delete-bin-6-line"></i></span>
+    <span class="tasklist__edit"><i class="ri-edit-2-line"></i></span></div>
+         <div class="delete">     <span class="tasklist__delete"> <i class="ri-delete-bin-6-line"></i></span></div>
 
     `;
   taskList.appendChild(newTask);
+  saveTaskToLocalStorage(taskText);
   taskInput.value = "";
+  return newTask;
 }
 
 addButton.addEventListener("click", addTask);
@@ -28,3 +34,33 @@ taskInput.addEventListener("keypress", (event) => {
     alert("Please enter a task.");
   }
 });
+
+taskList.addEventListener("click", (event) => {
+  if (event.target.closest(".tasklist__delete")) {
+    const task = event.target.closest(".tasklist__task");
+    task.remove();
+  }
+});
+
+function saveTaskToLocalStorage(taskText) {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push(taskText);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((taskText) => {
+    const newTask = document.createElement("div");
+    newTask.classList.add("tasklist__task");
+    newTask.innerHTML = `
+  <div class="tasklist__all">
+  <input type="checkbox" name="task" class="tasklist__checkbox" />
+    <p>${taskText}</p>
+    <span class="tasklist__edit"><i class="ri-edit-2-line"></i></span></div>
+         <div class="delete">     <span class="tasklist__delete"> <i class="ri-delete-bin-6-line"></i></span></div>
+
+    `;
+    taskList.appendChild(newTask);
+  });
+}
